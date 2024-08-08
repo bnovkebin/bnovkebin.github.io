81e588b58bea26838f9a6f0ba56aeb9c5a45fe18
bnovkebin <bnovkebin@gmail.com>
2024-08-08 16:36:55 +0900

    first-post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "First_post",
+        "excerpt":"Front Matter   title: “Jekyll 첫번째 글” categories: blog tags: jekyll  목차  toc: true  toc_sticky: true  —   ","categories": [],
+        "tags": [],
+        "url": "/first_post/",
+        "teaser": null
+      }]
