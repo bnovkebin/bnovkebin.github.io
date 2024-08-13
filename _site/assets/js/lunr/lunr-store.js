060f8a61f5248b0bd3df77429977272600f14cd2
bnovkebin <bnovkebin@gmail.com>
2024-08-13 23:20:52 +0900

    20240813

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "First_post",
-        "excerpt":"Front Matter   title: “Jekyll 첫번째 글” categories: blog tags: jekyll  목차  toc: true  toc_sticky: true  —   ","categories": [],
+        "excerpt":"Front Matter   title: “Jekyll first post”\\n This is my first post with github blog. I’ll share my interests in cyber security. My English skills aren’t very good, so if you find any errors, please don’t hesitate to tell me. categories: blog\\n tags: jekyll\\n  목차  toc: true\\n toc_sticky: true\\n  —   ","categories": [],
         "tags": [],
         "url": "/first_post/",
         "teaser": null
